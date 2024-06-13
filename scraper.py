f55bc69e7840ba62b9cf87233651c1b6dcc590c3
Burhanuddin Aliasghar <98301121+burhanuddin6@users.noreply.github.com>
2024-06-13 09:49:35 +0000

    sophisticated download

--- scraper.py
+++ scraper.py
@@ -1,32 +1,10 @@
-import os
-import subprocess
-import sys
-
-# Install Selenium
-
-# Install Chrome and ChromeDriver
-# os.system("sudo apt-get update")
-##### These lines do not work in the current environment #####
-# os.system("sudo apt-get install libxss1 libappindicator1 libindicator7")
-# os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
-# os.system("sudo apt install ./google-chrome*.deb")
-# os.system("CHROMEDRIVER_VERSION=$(curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE)")
-###############################################################################
-# # Just download the chromedriver that is compatible with the current Chrome version (or be geeky and automate it)
-# # here: https://developer.chrome.com/docs/chromedriver/downloads
-# os.system("wget -N https://storage.googleapis.com/chrome-for-testing-public/125.0.6422.141/linux64/chromedriver-linux64.zip")
-# os.system("unzip chromedriver-linux64.zip -d chromedriver")
-# os.system("rm chromedriver-linux64.zip")
-# os.system("sudo mv chromedriver/chromedriver-linux64/chromedriver /usr/local/bin/chromedriver")
-# os.system("rm -r chromedriver")
+from lib.drivers import check_and_install_chrome_driver
 
 # Selenium script to extract links
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-
-import google.generativeai as genai
 
 URL = "https://education.securiti.ai/certifications/privacyops/introduction/privacyops-overview/"
 CLASSNAME = "ld-tab-content"
@@ -68,5 +46,9 @@ def get_all_links(url: str, output_file: str) -> None:
 
 
 
+def main():
+    check_and_install_chrome_driver()
+    get_all_links(URL, "links.txt")
 
-get_all_links(URL, "links.txt")
+if __name__ == "__main__":
+    main()
